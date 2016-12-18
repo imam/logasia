714d6d9e5e27ed8f2d954b464fc3120f4fdd55b5
Imam Assidiqqi <Imam@iniilmu.com>
2016-12-18 09:08:27 +0700

    simple fix

--- database/seeds/VehiclesSeeder.php
+++ database/seeds/VehiclesSeeder.php
@@ -14,6 +14,7 @@ class VehiclesSeeder extends Seeder
     {
         factory(App\Date::class, 500)->make()->each(function($item, $key){
             $item->date= \App\Carbon::now()->setTime(0,0,0)->addDay($key);
+            $item->save();
             $item->semi_trailer_truck()->save(factory(App\SemiTrailerTruck::class)->make());
             $item->swap_body_truck()->save(factory(App\SwapBodyTruck::class)->make());
             $item->pup_trailer()->save(factory(App\PupTrailer::class)->make());
